8907ba29c36cb2afa67474c58437c8a72a7123dd
Neoceryx <dani.fieroo796@gmail.com>
2017-06-14 16:19:32 -0700

    Se cambio el titulo de la vista Test

--- application/views/TestView.php
+++ application/views/TestView.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Test View</title>
+
+    <title>Entraces And exits</title>
+
   </head>
   <body>
 
-    <h3>This is a Test View</h3>
+    <h3>Shift Entrances and exits</h3>
 
     <a href="<?=site_url('EmployeeController/test/123')?>">TesMethod</a>
 
