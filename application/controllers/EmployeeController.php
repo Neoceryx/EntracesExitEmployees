3088ad4166fb1770528e9e8c978c6916cfd0ffd1
Neoceryx <dani.fieroo796@gmail.com>
2017-06-14 14:21:23 -0700

    Proyect start

---
+++ application/controllers/EmployeeController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *
+ */
+class EmployeeController extends CI_Controller
+{
+
+  public function index()
+  {
+
+    // Load the View
+    $this->load->view('TestView');
+
+  }
+
+
+
+}
+
+
+?>
