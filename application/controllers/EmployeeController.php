f91171a7c422b0884ebac1b0fa4c5f48e8022080
Neoceryx <dani.fieroo796@gmail.com>
2017-06-15 11:16:13 -0700

    Se creo el metodo en el controllador Employee para mostrar el registro de las entradas y salidas y la diferencia de horas trbjadas

--- application/controllers/EmployeeController.php
+++ application/controllers/EmployeeController.php
@@ -9,36 +9,38 @@ class EmployeeController extends CI_Controller
   public function index()
   {
 
-    { /* Region Get info from Employees Model*/
-
-      // Load the Model
-      $this->load->model('Employees');
-
-      // Create Employees objet to access GetEmployees method.
-      $Result=$this->Employees->GetEmployees();
-
-      // Store the method result on an array
-      $Data= array('Employees' => $Result );
-
-    } /* End Region */
-
     // Load  the css filter_list
     $this->load->view('Template\css');
 
     // Load the Main View whit the Employees info
-    $this->load->view('TestView',$Data);
-
-    // Load the js files
-    $this->load->view("Template\js");
+    $this->load->view('EntranceExits');
 
   }
 
-  // Recive id value on the url.
-  public function test($id='')
+  public function EntrancesExitReport($value='')
   {
 
-  // Display the id val
-  echo $id;
+    // Build the query
+    $Query =$this->db->query("SELECT  Employees.NameEmp, Employees.FstName, Employees.NoEmploye, EntraceRegister, Exitregister, TIMEDIFF(EntraceRegister,Exitregister) AS 'TimeNeeded'
+    FROM  ShiftEntracesExits INNER JOIN Employees ON (Employees_Id = Id)");
+
+    // Store info in array
+    // $Data = array('Report' => $Query );
+
+    foreach ($Query->result() as $row ) {
+      echo "
+      <tr>
+        <td>".$row->NameEmp."</td>
+        <td>".$row->FstName."</td>
+        <td>".$row->NoEmploye."</td>
+        <td>".$row->EntraceRegister."</td>
+        <td>".$row->Exitregister."</td>
+        <td>".$row->TimeNeeded."</td>
+      </tr>
+      ";
+
+    }
+
 
   }
 
