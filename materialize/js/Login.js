f335fe37be9329694b67b0a745c13ed6d5d8197f
Neoceryx <dani.fieroo796@gmail.com>
2017-06-19 16:06:27 -0700

    Se creo la funcion para enviar los valores del formulario de logion al metodo en el controlados

--- materialize/js/Login.js
+++ materialize/js/Login.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
-  alert("Hi From Login.js");
+
+  // Set the main path on the server
+  let URL="http://localhost/EntracesExitEmployees/index.php/"
+
+
+  $("#js_LoginBtn").click(function () {
+
+    // Get Employee Number
+    var EmpNumber=$("#js_EmployeeNmbr").val();
+
+    // Get Employe Password
+    var Password=$("#js_Password").val();
+
+    // Start ajax
+    $.ajax({
+      type:"POST",
+      url:URL+"LoginController/Login",
+      data:{ EMPNUM:EmpNumber, PASS:Password },
+      success:function (data) {
+
+        // Display backend result on the DOM
+        $(".Js_LgnResult").html(data);
+
+      },
+      error:function (xhr) {
+        alert("Error: " +xhr.responseText);
+      }
+
+    });
+    // End ajax
+
+  });
+  // End Click
+
 });
 // End Scope
