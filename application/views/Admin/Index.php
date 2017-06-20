422b1ef966a3c8e227f6c85b79f81f699c7f98df
Neoceryx <dani.fieroo796@gmail.com>
2017-06-20 11:53:16 -0700

    Se creo el card que llevara al admin a la vista de Entrancesreports

--- application/views/Admin/Index.php
+++ application/views/Admin/Index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title> Admin DashBoard</title>
   </head>
   <body>
 
@@ -19,7 +19,7 @@
           <!-- Validate If the user is login -->
           <?php if ($this->session->userdata('login') ) { ?>
 
-            <li><a href="<?=site_url('LoginController/LogOut')?>">Log Out</a></li>      
+            <li><a href="<?=site_url('LoginController/LogOut')?>">Log Out</a></li>
 
           <?php } else { ?>
 
@@ -36,7 +36,7 @@
     <!-- End NavBar -->
 
 
-    <h3>
+    <h3 class="center-align">
       Welcome
 
       <?php foreach ($Employee->result() as $employe) {?>
@@ -49,9 +49,41 @@
 
     </h3>
 
+    <!-- Start Content -->
+    <div class="row">
+      <div class="col s12 m12 l12">
+
+        <!-- Start Card Entrances Report -->
+        <div class="row">
+          <div class="col s12 m4 l4">
+            <div class="card-panel teal">
+
+              <p class="center-align">
+                <i class="material-icons white-text large ">&#xE865;</i>
+              </p>
+
+              <h4 class="white-text center-align">Entrances Report</h4>
+
+            </div>
+          </div>
+        </div>
+        <!-- End Card Entrances Report -->
 
 
-
+      </div>
+    </div>
+    <!-- End Content -->
 
   </body>
+
+  <!-- Jquery  -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+  <!-- Materialize js Files -->
+  <script type="text/javascript" src="<?=base_url('materialize\js\materialize.min.js')?>"></script>
+
+  <!-- Aditional js -->
+  <script type="text/javascript" src="<?=base_url('materialize\js\Admin\Index.js')?>"></script>
+
+
 </html>
