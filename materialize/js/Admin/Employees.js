503626b35a9d9edccc2907ae9a82099384a86d8e
Neoceryx <dani.fieroo796@gmail.com>
2017-06-26 13:37:38 -0700

    Se creo la llamada ajax que obtiene la lista de los empleados del controlador employees

--- materialize/js/Admin/Employees.js
+++ materialize/js/Admin/Employees.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
 
+  // Set the main path on the server
+  let URL="http://localhost/EntracesExitEmployees/index.php/"
+
+  // Display all employees
+  $.ajax({
+    type:"POST",
+    url:URL+"EmployeeController/GetEmployeesList",
+    data:{},
+    success:function (data) {
+
+      // Display backend result in the dom
+      $(".js_EmpList").html(data);
+
+    },
+    error:function (xhr) {
+
+      alert("Error: " +xhr.responseText);
+
+    }
+  });
+
   $(".js_Employee").click(function () {
 
     // Get Employe Id
