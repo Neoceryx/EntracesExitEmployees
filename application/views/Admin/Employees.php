cfec7a0867715bbf0e9155f5304022cabfc2b8c3
Neoceryx <dani.fieroo796@gmail.com>
2017-06-26 09:57:13 -0700

    Se comenzo con la vista que mostrara a todos los empleados

---
+++ application/views/Admin/Employees.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <h3 class="center-align">Employees List</h3>
+
+    <!-- Start employee list container -->
+    <div class="row">
+
+      <!-- Start Employee Card -->
+      <div class="col s12 m5">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+      <!-- End Employee Card -->
+
+
+    </div>
+  <!-- End employee list container -->
+
+  </body>
+
+  <!-- Jquery  -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+  <!-- Materialize js Files -->
+  <script type="text/javascript" src="<?=base_url('materialize\js\materialize.min.js')?>"></script>
+
+
+
+</html>
