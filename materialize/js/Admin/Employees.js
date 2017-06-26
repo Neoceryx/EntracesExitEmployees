457d4a817f912bb99bd3475182094aa68e558cf4
Neoceryx <dani.fieroo796@gmail.com>
2017-06-26 14:54:18 -0700

    Se creo el metodo para buscar por nombre de empleado

--- materialize/js/Admin/Employees.js
+++ materialize/js/Admin/Employees.js
@@ -3,32 +3,52 @@ $(document).ready(function () {
   // Set the main path on the server
   let URL="http://localhost/EntracesExitEmployees/index.php/"
 
-  // Display all employees
-  $.ajax({
-    type:"POST",
-    url:URL+"EmployeeController/GetEmployeesList",
-    data:{},
-    success:function (data) {
-
-      // Display backend result in the dom
-      $(".js_EmpList").html(data);
-
-    },
-    error:function (xhr) {
-
-      alert("Error: " +xhr.responseText);
-
-    }
-  });
-
+  // Redirect to Employee Details View
   $(".js_Employee").click(function () {
 
     // Get Employe Id
     var EmpId=$(this).data("empid");
-    debugger;
+
+    // Get Employe name in lower case
+    var EmName=$(this).find("#js_EmpName").text().toLowerCase();
 
   });
 
+  // Seacrh
+  $("#js_SrchBtn").click(function () {
+
+    // Get and convert the search string val to lowercase
+    var Srchstring=$("#js_SrchString").val().toLowerCase();
+    console.log( typeof(Srchstring) );
+
+    // Iterates over Employees list
+    $(".js_Employee").each(function () {
+
+      // Get the element
+      var Item=$(this);
+
+      // Get and convert the employees names to lower case
+      var NamesFull=$(this).find(".js_EmpName").text().toLowerCase();
+
+      // Separate the FstNme to the Name
+      var SplitNames=NamesFull.split(" ");
+
+      // Get only the name part
+      var NamePart=SplitNames[0];
+
+      // Validate Employees names
+      if (NamePart != Srchstring) {
+
+        // Hide the element.
+        Item.hide();
+
+      }
+
+    });
+    // End each
+
+  });
+  // End click
 
 });
 // End Scope
