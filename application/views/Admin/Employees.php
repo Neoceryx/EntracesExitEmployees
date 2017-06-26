aa581afbea3078e30bc1c3c89d7ce149340418eb
Neoceryx <dani.fieroo796@gmail.com>
2017-06-26 13:35:59 -0700

    Se creo el emeneto en la vista de employees para mostrar la lista de los empleados

--- application/views/Admin/Employees.php
+++ application/views/Admin/Employees.php
@@ -9,28 +9,7 @@
     <h3 class="center-align">Employees List</h3>
 
     <!-- Start employee list container -->
-    <div class="row">
-
-      <!-- Start Employee Card -->
-      <div class="col s12 m4 l4 js_Employee" data-empid="1">
-          <div class="card-panel grey lighten-5 z-depth-1">
-            <div class="row valign-wrapper">
-              <div class="col s4">
-                <img src="https://cdn0.iconfinder.com/data/icons/superuser-web-kit/512/686909-user_people_man_human_head_person-512.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-              </div>
-              <div class="col s8">
-                <span class="black-text">
-                  <p>Daniel Fierro</p>
-                  <p>12345678</p>
-                  <p>Normal</p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      <!-- End Employee Card -->
-
-    </div>
+    <div class="row js_EmpList"></div>
   <!-- End employee list container -->
 
   </body>
