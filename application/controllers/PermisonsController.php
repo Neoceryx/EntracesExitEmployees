f6495de58636a801fd3eb3c308b2c18cc1b03e97
Neoceryx <dani.fieroo796@gmail.com>
2017-07-06 11:17:15 -0700

    Se agrego la consulta que obtiene la lista de permisos y envia la info a la vista

--- application/controllers/PermisonsController.php
+++ application/controllers/PermisonsController.php
@@ -12,8 +12,14 @@ class PermisonsController extends CI_Controller
     // Load css files
     $this->load->view("Template\css");
 
-    // return the view
-    $this->load->view("Employees/EmployeesPermisons");
+    // Build the query
+    $query = $this->db->get('permisontypes');
+
+    // Store query result into assosiative array
+    $Data = array('PermisonsTypes' => $query);
+
+    // return the view. whit the database info
+    $this->load->view("Employees/EmployeesPermisons", $Data);
 
   }
 
