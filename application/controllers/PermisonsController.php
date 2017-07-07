3018c03978ddc5ed2636d76517e2ca6bba7e247e
raul <auxsistemas@maypa.mx>
2017-07-07 12:44:19 -0700

    se cargo el metogo que obtiene la lista de los tipos de permisos en el metodo del controlador de permisos

--- application/controllers/PermisonsController.php
+++ application/controllers/PermisonsController.php
@@ -12,11 +12,14 @@ class PermisonsController extends CI_Controller
     // Load css files
     $this->load->view("Template\css");
 
-    // Build the query
-    $query = $this->db->get('permisontypes');
+    // Load the model
+    $this->load->model('Permisons');
+
+    // Acces to model method
+    $Result=$this->Permisons->GetPermisonsTypesList();
 
     // Store query result into assosiative array
-    $Data = array('PermisonsTypes' => $query);
+    $Data = array('PermisonsTypes' => $Result);
 
     // return the view. whit the database info
     $this->load->view("Employees/EmployeesPermisons", $Data);
