18e47eea922c7cc96b99f007542aebce43685d72
raul <auxsistemas@maypa.mx>
2017-07-07 16:07:51 -0700

    Se agregaron las consultas pr obtener el id del empleado por numero de emplado y la consulta para registrar la hora y fecha que un empleado solicito un permiso

--- application/controllers/PermisonsController.php
+++ application/controllers/PermisonsController.php
@@ -26,6 +26,61 @@ class PermisonsController extends CI_Controller
 
   }
 
+  public function RegiterPermison()
+  {
+
+    // Get Permison Id from ajax call
+    $PerId=$this->input->post("PERMID");
+
+    // Get Employee number from ajax call
+    $EmpNumber=$this->input->post("EMNUMBER");
+
+    // Variables
+    $EmpId=0;
+
+    // echo $PerId. " :: ". $EmpNumber;
+
+    { /* Region Get EmployeId by EmpNumber */
+
+      // Build the query
+      $this->db->select('Id');
+      $this->db->from('employees');
+      $this->db->where('NoEmploye', $EmpNumber);
+
+      // prepre query to be executed
+      $Query=$this->db->get();
+
+      // Get DataBase info
+      foreach ($Query->result() as $employee) {
+         $EmpId=$employee->Id;
+      }
+
+    } /* End Region */
+
+    { /* Region Add Employee Permison */
+
+      // Get the current date
+      $CurrentDate = date('Y-m-d H:i:s');
+
+      // Assosiative array
+      $Data = array(
+        'Employees_Id' => $EmpId,
+        'PermisonTypes_Id' => $PerId,
+        'Description' =>'asd' ,
+        'StartDate' => $CurrentDate,
+        'EndDate' => '',
+      );
+
+      // Build the query
+      $this->db->insert('permisons',$Data);
+
+
+
+    } /* End Region */
+
+  }
+  // End funtion
+
 
 }
 // End Controller Class
