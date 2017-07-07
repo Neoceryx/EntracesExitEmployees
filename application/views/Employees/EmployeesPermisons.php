d3ff1bd64f272de4013e0b83f738fc17c84770e6
raul <auxsistemas@maypa.mx>
2017-07-07 13:33:01 -0700

    Se creo la validacion para colocar el icono correcto a los tipos de permisos

--- application/views/Employees/EmployeesPermisons.php
+++ application/views/Employees/EmployeesPermisons.php
@@ -14,20 +14,85 @@
 
     <h3 class="center-align">Production Permisons</h3>
 
-    <!-- Display backend results -->
-    <?php foreach ($PermisonsTypes->result() as $row) {  ?>
+    <!-- Start Permisons types lits -->
+    <div class="row">
 
-      <p class="js_a"> <?=$row->Id. " :: ". $row->Description ?> </p>
+      <!-- Add 1 space blank to the right to center permisons types -->
+      <div class="col s12 m1"></div>
 
-    <?php } ?>
-    <!-- End foreach -->
+      <!-- Display backend results -->
+      <?php foreach ($PermisonsTypes->result() as $row) {  ?>
+
+        <!-- Start Permison type card -->
+        <div class="col s12 m2 js_a"  data-permd="<?=$row->Id?>">
+          <div class="card-panel teal">
+
+            <?php
+
+            // Validate permison id to add the icon correct
+            switch ($row->Id) {
+
+              case 1:
+
+                echo "
+                <div class='center-align white-text'>
+                <i class='material-icons medium'>&#xE63D;</i>
+                </div>";
+
+              break;
+
+              case 2:
+
+              echo "
+              <div class='center-align white-text'>
+              <i class='material-icons medium'>&#xE561;</i>
+              </div>";
+
+              break;
+
+              case 3:
+
+              echo "
+              <div class='center-align white-text'>
+              <i class='material-icons medium'>&#xE853;</i>
+              </div>";
+
+              break;
+
+              case 4:
+
+              echo "
+              <div class='center-align white-text'>
+              <i class='material-icons medium'>&#xE548;</i>
+              </div>";
+
+              break;
+
+              default:
+
+              echo "
+              <div class='center-align white-text'>
+              <i class='material-icons medium'>&#xE645;</i>
+              </div>";
+
+              break;
+            }
+
+             ?>
+
+            <h4 class="white-text center-align"><?=$row->Description?></h4>
+
+          </div>
+        </div>
+        <!-- Start Permison type card -->
+
+      <?php } ?>
+      <!-- End foreach -->
 
 
 
-
-
-
-
+    </div>
+    <!-- End Permisons types list -->
 
   </body>
 
