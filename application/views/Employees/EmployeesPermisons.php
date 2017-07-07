85d97d6509cb39447c9b9446e414878d83bb8f3e
raul <auxsistemas@maypa.mx>
2017-07-07 14:12:01 -0700

    Se agregaron los plugin de qr reder y alertfy en la vista de Employees producctions Permisons

--- application/views/Employees/EmployeesPermisons.php
+++ application/views/Employees/EmployeesPermisons.php
@@ -3,14 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Productions Permisons</title>
+
+    <!-- Alertfy css Files -->
+    <link rel="stylesheet" href="<?=base_url("materialize\Plugins\alertifyjs\css\alertify.min.css")?>">
+    <link rel="stylesheet" href="<?=base_url("materialize\Plugins\alertifyjs\css/themes\default.min.css")?>">
+
   </head>
   <body>
-
-    <!-- Start  -->
-    <div class="row">
-
-    </div>
-
 
     <h3 class="center-align">Production Permisons</h3>
 
@@ -89,10 +88,55 @@
       <?php } ?>
       <!-- End foreach -->
 
+    </div>
+    <!-- End Permisons types list -->
+
+    <!-- Start qr reader container -->
+    <div class="row">
+
+      <!-- Add 1 blank sapace to the lef to adjust the Qr reader and table permisons -->
+      <div class="col s12 m1"></div>
+
+      <!-- Start Web Cam container -->
+      <div class="col s12 m5">
+
+        <!-- Start web cam controlls -->
+        <div>
+
+          <button id="js_Play"type="button" name="button" class="btn waves-effect waves-light">
+            <i class="material-icons left">&#xE038;</i>
+            Play
+          </button>
+
+          <button id="js_Stop" type="button" name="button" class="btn red waves-effect waves-light">
+            <i class="material-icons left">&#xE047;</i>
+            Stop
+          </button>
+
+        </div>
+        <!-- End web cam controlls -->
+
+
+        <!-- Web Cam Decoder -->
+        <canvas style="margin-top:2%"></canvas>
+
+        <!-- Display the code val decoded -->
+        <div id="js_Coderesult"></div>
+
+      </div>
+      <!-- Start Web Cam container -->
+
+
+      <div class="col s12 m5">
+
+        Table Permisons
+
+      </div>
+
 
 
     </div>
-    <!-- End Permisons types list -->
+    <!-- End qr reader container -->
 
   </body>
 
@@ -114,6 +158,13 @@
 
   <!-- Aditional js -->
   <script type="text/javascript" src="<?=base_url('materialize\js\Employees\Production.js')?>"></script>
+
+  <!-- Codes Decoder js files -->
+  <script type="text/javascript" src="<?=base_url('materialize\Plugins\webcodecamjs-master\js\qrcodelib.js')?>"></script>
+  <script type="text/javascript" src="<?=base_url('materialize\Plugins\webcodecamjs-master\js\webcodecamjquery.js')?>"></script>
+
+  <!-- Alertfy js -->
+  <script src="<?=base_url("materialize\Plugins\alertifyjs\alertify.min.js")?>"></script>
 
 
 </html>
